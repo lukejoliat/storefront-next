9a23c2c013ac583df06430f94abc66271b8fe73e
lukejoliat <lukejoliat@gmail.com>
2023-04-19 22:35:39 -0400

    Add changes

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,32 +3,32 @@ import { Main } from "@/components/main";
 import { SideBar } from "@/components/sidebar";
 import { ProductList } from "./product-list";
 import { ProductFilters } from "./product-filters";
-import { ReadonlyURLSearchParams, useSearchParams } from "next/navigation";
+import { ReadonlyURLSearchParams } from "next/navigation";
 import { Suspense } from "react";
 import Loading from "./loading";
 
 export enum Filters {
-  NAME = "name",
-  PRICE_FROM = "price-from",
-  PRICE_TO = "price-to",
+    NAME = "name",
+    PRICE_FROM = "price-from",
+    PRICE_TO = "price-to",
 }
 
 export default async function Page({
-  searchParams,
+    searchParams,
 }: {
-  searchParams: ReadonlyURLSearchParams;
+    searchParams: ReadonlyURLSearchParams;
 }) {
-  return (
-    <Container layout="horizontal">
-      <SideBar>
-        <ProductFilters />
-      </SideBar>
-      <Main>
-        <Suspense fallback={<Loading />}>
-          {/* @ts-expect-error Async Server Component */}
-          <ProductList searchParams={searchParams} />
-        </Suspense>
-      </Main>
-    </Container>
-  );
+    return (
+        <Container layout="horizontal">
+            <SideBar>
+                <ProductFilters />
+            </SideBar>
+            <Main>
+                <Suspense fallback={<Loading />}>
+                    {/* @ts-expect-error Async Server Component */}
+                    <ProductList searchParams={searchParams} />
+                </Suspense>
+            </Main>
+        </Container>
+    );
 }
